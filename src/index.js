f958184b9db274c0e0f01da037aada107671f722
Dauniusha <78425928+Dauniusha@users.noreply.github.com>
2021-03-13 16:45:14 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length - 1; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          if (str[i] == bracketsConfig[j][0] && str[i + 1] == bracketsConfig[j][1]) {
+              str = str.slice(0, i) + str.slice(i + 2);
+              i = -1;
+          }
+      }
+  }
+  if (!str)
+      return true;
+  else
+      return false;
 }
